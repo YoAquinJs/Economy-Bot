909b1c3d31f16093b4badff4fa486b4dff0bc121
Erik Ivanov <erik.ivanov.n@gmail.com>
2021-07-02 22:21:31 -0500

    test_transferir

--- tests/simulations.py
+++ tests/simulations.py
@@ -1,7 +1,7 @@
 import discord
 
 
-async def simulate_registration(dpytest, member: discord.Member):
+async def simulate_registration(dpytest, member: discord.Member) -> str:
     """Simula el registro de un usuario y regresa la descripcion del embed
 
     Args:
@@ -18,7 +18,7 @@ async def simulate_registration(dpytest, member: discord.Member):
     return emb_desc
 
 
-async def simulate_desregistration(dpytest, member: discord.Member):
+async def simulate_desregistration(dpytest, member: discord.Member) -> str:
     """Simula el desregistro de un usuario y regresa la descripcion del embed
 
     Args:
@@ -35,13 +35,16 @@ async def simulate_desregistration(dpytest, member: discord.Member):
     return emb_desc
 
 
-async def simulate_imprimir_monedas(dpytest, quantity: float ,member: discord.Member, receiving_member: discord.Member):
+async def simulate_imprimir_monedas(dpytest, quantity: float, member: discord.Member, receiving_member: discord.Member) -> str:
     """Hace una simulacion del comando !imprimir
 
     Args:
         dpytest (module): referencia al modulo de dpytest
         member (discord.Member): Mienbro que va a imprimir las monedas (Tiene que tener permisos de administrador)
         receiving_member (discord.Member): Miembo al que se le van a dar las monedas impresas
+
+    Returns:
+        str: embed description
     """
     await dpytest.empty_queue()
     await dpytest.message(f'!imprimir {quantity} {receiving_member.mention}', member=member)
@@ -49,12 +52,15 @@ async def simulate_imprimir_monedas(dpytest, quantity: float ,member: discord.Me
     return dpytest.get_embed().description
 
 
-async def simulate_ver_monedas(dpytest, member: discord.Member):
+async def simulate_ver_monedas(dpytest, member: discord.Member) -> str:
     """Simula el comando !monedas para obtener el numero de monedas de member
 
     Args:
         dpytest (module): referencia al modulo de dpytest
         member (discord.Member): Mienbro que usara el comando !monedas
+
+    Returns:
+        str: embed description
     """
     await dpytest.empty_queue()
     await dpytest.message(f'!monedas {member.mention}', member=member)
@@ -62,8 +68,37 @@ async def simulate_ver_monedas(dpytest, member: discord.Member):
     return dpytest.get_embed().description
 
 
-async def simulate_expropiar(dpytest, quantity: float ,member: discord.Member):
+async def simulate_expropiar(dpytest, quantity: float,member: discord.Member, admin: discord.Member) -> str:
+    """Simula el comando !expropiar
+
+    Args:
+        dpytest (module): referencia al modulo de dpytest
+        quantity (float): cantidad a expropiar
+        admin (discord.Member): mienbro con permisos de administrador
+        member (discord.Member): miembro al que se le va a expropiar
+
+    Returns:
+        str: embed description
+    """
     await dpytest.empty_queue()
-    await dpytest.message(f'!expropiar {quantity} {member.mention}', member=member)
+    await dpytest.message(f'!expropiar {quantity} {member.mention}', member=admin)
+
+    return dpytest.get_embed().description
+
+
+async def simulate_transferir(dpytest, member: discord.Member, quantity: float, receptor: discord.member) -> str:
+    """Sumula el comando !transferir
+
+    Args:
+        dpytest (module): [description]
+        member (discord.Member): miembro que va enviar monedas
+        quantity (float): cantidad a enviar
+        receptor (discord.member): mienbro que va a recibir monedas
+
+    Returns:
+        str: embed description
+    """
+    await dpytest.empty_queue()
+    await dpytest.message(f'!transferir {quantity} {receptor.mention}', member=member)
 
     return dpytest.get_embed().description
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -26,7 +26,7 @@ async def bot(event_loop):
     guild = dpytest.get_config().guilds[0]
     db_utils.delete_database_guild(guild)
 
-    admin_user = dpytest.backend.make_user(username='Test', discrim=1)
+    admin_user = dpytest.backend.make_user(username='Admin', discrim=1)
     admin_member = dpytest.backend.make_member(admin_user, guild)
     admin_role = await guild.create_role(name="Admin", permissions=Permissions.all())
     await admin_member.add_roles(admin_role)
@@ -61,7 +61,7 @@ async def test_desregistro(bot):
 @pytest.mark.asyncio
 async def test_imprimir(bot):
     await simulate_registration(dpytest, admin_member)
-    print_embed_desc = await simulate_imprimir_monedas(dpytest, 200,admin_member, admin_member)
+    print_embed_desc = await simulate_imprimir_monedas(dpytest, 200, admin_member, admin_member)
     desired_print_embed_desc = f'se imprimieron 200.0, y se le asignaron a {admin_member.display_name}, id {admin_user.id}'
 
     assert print_embed_desc == desired_print_embed_desc
@@ -76,7 +76,7 @@ async def test_imprimir(bot):
 @pytest.mark.asyncio
 async def test_expropiar(bot):
     await simulate_registration(dpytest, admin_member)
-    print_embed_desc = await simulate_imprimir_monedas(dpytest, 200,admin_member, admin_member)
+    print_embed_desc = await simulate_imprimir_monedas(dpytest, 200, admin_member, admin_member)
     desired_print_embed_desc = f'se imprimieron 200.0, y se le asignaron a {admin_member.display_name}, id {admin_user.id}'
 
     assert print_embed_desc == desired_print_embed_desc
@@ -86,7 +86,7 @@ async def test_expropiar(bot):
 
     assert monedas_embed_desc == desired_monedas_embed_desc
 
-    expropiar_embed_desc = await simulate_expropiar(dpytest, 10.75, admin_member)
+    expropiar_embed_desc = await simulate_expropiar(dpytest, 10.75, admin_member, admin_member)
     desired_expropiar_embed_desc = f'se le expropiaron 10.75 monedas a {admin_user.name}, id {admin_user.id}'
 
     assert expropiar_embed_desc == desired_expropiar_embed_desc
@@ -96,4 +96,35 @@ async def test_expropiar(bot):
 
     assert monedas_embed_desc == desired_monedas_embed_desc
 
+    db_utils.delete_database_guild(guild)
+
+
+@pytest.mark.asyncio
+async def test_transferir(bot):
+    await simulate_registration(dpytest, admin_member)
+    print_embed_desc = await simulate_imprimir_monedas(dpytest, 200, admin_member, admin_member)
+    desired_print_embed_desc = f'se imprimieron 200.0, y se le asignaron a {admin_member.display_name}, id {admin_user.id}'
+    assert print_embed_desc == desired_print_embed_desc
+
+    # Crea un nuevo usuario para simular una transferencia
+    test_user = dpytest.backend.make_user(username='Test', discrim=2)
+    test_member = dpytest.backend.make_member(test_user, guild)
+    await simulate_registration(dpytest, test_member)
+
+    trasferir_embed_desc = await simulate_transferir(dpytest, admin_member, 50.5, test_member)
+    desired_trasferir_embed_desc = 'transaccion completa'
+
+    assert trasferir_embed_desc == desired_trasferir_embed_desc
+
+    # Ver si salen las cuentas en el admin
+    balance_admin = await simulate_ver_monedas(dpytest, admin_member)
+    desired_balance_admin = f'tienes 149.5 bonobo coins {admin_user.name}'
+
+    assert balance_admin == desired_balance_admin
+
+    # ver si salen las cuentas del usuario test
+    balance_test = await simulate_ver_monedas(dpytest, test_member)
+    desired_balance_test = f'tienes 50.5 bonobo coins {test_user.name}'
+
+    assert balance_test == desired_balance_test
     db_utils.delete_database_guild(guild)
