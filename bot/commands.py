069bc9aeaab91a8ff6f9f80af41bbbc47bb32bcb
YoAquinJS <sebascertuche@gmail.com>
2021-09-12 10:09:15 -0500

    Confirmacion De Actividad agregada

--- bot/commands.py
+++ bot/commands.py
@@ -49,6 +49,16 @@ async def ping_chek(ctx: Context):
     await send_message(ctx, f"Latencia: {int(round(client.latency * 1000, 0))}ms")
 
 
+@client.command(name="checkifawake")
+async def ping_chek(ctx: Context):
+    """Confirma que el bot esta activo con una reacccion
+
+    Args:
+        ctx (Context): Context de discord
+    """
+    await ctx.message.add_reaction("✅")
+
+
 @slash.slash(name="bug", guild_ids=guild_ids, description="reporta un bug del bot a los desarrolladores",
              options=[
               create_option(name="comando", description="nombre del comando o sistema que ocasiono el bug",
